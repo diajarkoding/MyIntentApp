0a2158fac2b2dbf15ade75ef095ac5f5b0469844
Iskandar Muhaemin <iskandar.muhaemin@danasaham.co.id>
2024-07-02 10:24:38 +0700

    menggunakan parcelize untuk membuat object / parcelable

--- app/src/main/java/com/example/myintentapp/Person.kt
+++ app/src/main/java/com/example/myintentapp/Person.kt
@@ -2,7 +2,10 @@ package com.example.myintentapp
 
 import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parceler
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class Person (
     val name: String?,
     val age: Int?,
@@ -17,24 +20,17 @@ data class Person (
     ) {
     }
 
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(name)
-        parcel.writeValue(age)
-        parcel.writeString(email)
-        parcel.writeString(city)
-    }
+    companion object : Parceler<Person> {
 
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Person> {
-        override fun createFromParcel(parcel: Parcel): Person {
-            return Person(parcel)
+        override fun Person.write(parcel: Parcel, flags: Int) {
+            parcel.writeString(name)
+            parcel.writeValue(age)
+            parcel.writeString(email)
+            parcel.writeString(city)
         }
 
-        override fun newArray(size: Int): Array<Person?> {
-            return arrayOfNulls(size)
+        override fun create(parcel: Parcel): Person {
+            return Person(parcel)
         }
     }
 }
